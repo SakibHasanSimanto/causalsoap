c540f3da9d144e9767421518270bea23e177be94
Kazi Sakib Hasan <156543263+SakibHasanSimanto@users.noreply.github.com>
2025-06-22 19:21:13 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deepCausal',
+    version='0.1.0',
+    author='Kazi Sakib Hasan',
+    author_email='simanto.alt@gmail.com',
+    description='A causal feature selection library using residual-based ATE estimation.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/deepCausal',  # i need to update it when repo is ready
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
